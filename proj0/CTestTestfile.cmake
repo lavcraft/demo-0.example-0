e1f6ee898ce66643f1b1088c7974ebf3c86ef954
Kirill Tilkachev <lavcraft@gmail.com>
2015-06-09 23:32:19 +0300

    delte build artifact and exclude CMakeTest.cmake

--- proj0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tolkv/git/cmake-dependency-proj0/proj0
-# Build directory: /Users/tolkv/git/cmake-dependency-proj0/proj0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(Proj0Run "/Users/tolkv/git/cmake-dependency-proj0/bin/proj0app")
