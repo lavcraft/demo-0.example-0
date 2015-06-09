8dbe3caaeaaaa0bab7de0976f6e7ec8684d14c1e
Kirill Tilkachev <lavcraft@gmail.com>
2015-06-10 00:17:32 +0300

    Change CTest config: set url to local cdash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,8 +1,15 @@
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
 set(CTEST_PROJECT_NAME "agat-demo-0")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "ec2-52-11-58-165.us-west-2.compute.amazonaws.com")
+set(CTEST_DROP_SITE "cdash.agat")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=agat-demo-0")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
--- proj0/CTestConfig.cmake
+++ proj0/CTestConfig.cmake
@@ -11,8 +11,12 @@ ctest_submit(PARTS Build)
 ctest_test(BUILD “${CTEST_BINARY_DIRECTORY}”)
 ctest_submit(PARTS Test)
 
+set(CTEST_PROJECT_NAME "agat-demo-0")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "ec2-52-11-58-165.us-west-2.compute.amazonaws.com")
+set(CTEST_DROP_SITE "cdash.agat")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=agat-demo-0")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
 ctest_submit()
